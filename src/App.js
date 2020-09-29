364a194d0b138fb50c8bacb16a14091a93318625
ali <weplutus.1@gmail.com>
2020-09-29 19:35:27 +0300

    orgnize

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import TheTitleField from './SVGs/TheTitleField.svg'
 import './App.css';
 import Title from './Components/Title';
-
+// AIzaSyBBtnznBl_zwAXJNQKHFIE6pouAmORZ0Kg
 function App() {
   const ref = useRef()
   const image = useRef()
@@ -14,18 +14,20 @@ function App() {
     image.current.offsetWidth > image.current.offsetHeight ? setStyles({ height: `${image.current.width}px`, width: `${image.current.height}px` }) : setStyles({ height: '500px' })
   }
   return (
-    <div
-      onMouseOver={() => ref.current.focus()}
-      className="App">
-
-      <div className='outline'></div>
+    <div>
       <div
-        className='container'>
+        onMouseOver={() => ref.current.focus()}
+        className="App">
 
-        {Title(ref)}
-        <img className='field' src={TheTitleField} alt={TheTitleField} />
-        <input onChange={handleChange} placeholder='add image url here.' />
-        <img ref={image} style={styles} className='image' src={state} alt='https://arbordayblog.org/wp-content/uploads/2018/06/oak-tree-sunset-iStock-477164218.jpg' />
+        <div className='outline'></div>
+        <div
+          className='container'>
+
+          {Title(ref)}
+          <img className='field' src={TheTitleField} alt={TheTitleField} />
+          <input onChange={handleChange} placeholder='add image url here.' />
+          <img ref={image} style={styles} className='image' src={state} alt='https://arbordayblog.org/wp-content/uploads/2018/06/oak-tree-sunset-iStock-477164218.jpg' />
+        </div>
       </div>
     </div>
   );

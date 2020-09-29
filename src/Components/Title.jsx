7579d0a53efca24b0090a02e0c2df095c0526df3
ali <weplutus.1@gmail.com>
2020-09-29 15:41:17 +0300

    beta version

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import DragResizeContainer from 'react-drag-resize';
 
 function Title(ref) {
+      // const ref = useRef()
       const [isOver, setOver] = useState(false)
-      // console.log(isOver)
+      const [font, setFont] = useState(40)
+      function handlChange(e) {
+            console.log(ref.current.innerText.length)
+            ref.current.innerText.length > 13 ? setFont(30) : setFont(40)
+            ref.current.innerText.length > 16 ? setFont(20) : setFont(40)
+      }
+
       return (
             <div onMouseOver={e => setOver(true)} onMouseLeave={e => setOver(false)}
                   className='title-containter'>
                   <div
+                        style={{ fontSize: `${font}px` }}
+                        onKeyUp={handlChange}
                         ref={ref}
                         placeholder='add text here'
                         className='title'

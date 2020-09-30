c02ef527075a305903a823b031e54e0bcfce70d9
ali <weplutus.1@gmail.com>
2020-09-30 13:11:14 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,81 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import TheTitleField from './SVGs/TheTitleField.svg'
 import './App.css';
 import Title from './Components/Title';
-// AIzaSyBBtnznBl_zwAXJNQKHFIE6pouAmORZ0Kg
+import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from "react-component-export-image";
+import { Rnd } from 'react-rnd';
+
+
+
 function App() {
+  const box = useRef()
   const ref = useRef()
   const image = useRef()
   const [state, setstate] = useState('https://arbordayblog.org/wp-content/uploads/2018/06/oak-tree-sunset-iStock-477164218.jpg')
   const [styles, setStyles] = useState()
-
+  const [isOver, setOver] = useState(false)
+  const [tooLong, setLong] = useState(false)
+  const [h, setH] = useState(true)
   function handleChange(e) {
     setstate(e.target.value)
     image.current.offsetWidth > image.current.offsetHeight ? setStyles({ height: `${image.current.width}px`, width: `${image.current.height}px` }) : setStyles({ height: '500px' })
+    setH(image.current.height > image.current.width)
+  }
+  function handlDrag(e) {
+    if (h) setStyles({ left: e.pageX - image.current.width / 2 })
+    if (!h) setStyles(pre => {
+      return { ...pre, top: e.pageY - image.current.height / 2 }
+    })
+
+
   }
   return (
-    <div>
+
+    <div >
       <div
+
+        onMouseLeave={() => setOver(false)} onMouseOverCapture={() => setOver(true)}
         onMouseOver={() => ref.current.focus()}
         className="App">
-
         <div className='outline'></div>
-        <div
-          className='container'>
+        <div ref={box} className='container'>
+          {Title(ref, setOver, setLong, tooLong)}
+          {!tooLong && <img className='field' src={TheTitleField} alt={TheTitleField} />}
+          <div style={{ zIndex: '11', position: 'relative', transform: `scale(${isOver ? 1 : 0})` }} className=''>
+            <Rnd
+              // default={{ resizeHandleWrapperStyle: false }}
+              handle=".handle"
+              position={null}
+              grid={[25, 25]}
+              scale={1}
+              onDrag={handlDrag}
+            >
+              <IconButton style={{ backgroundColor: 'white', color: 'tomato' }} variant="contained" color="default" className='handle' >
+                <DragIndicatorIcon />
+              </IconButton>
+            </Rnd>
+          </div>
+          <img ref={image} style={styles} className='image handle' src={state} alt='https://arbordayblog.org/wp-content/uploads/2018/06/oak-tree-sunset-iStock-477164218.jpg' />
 
-          {Title(ref)}
-          <img className='field' src={TheTitleField} alt={TheTitleField} />
-          <input onChange={handleChange} placeholder='add image url here.' />
-          <img ref={image} style={styles} className='image' src={state} alt='https://arbordayblog.org/wp-content/uploads/2018/06/oak-tree-sunset-iStock-477164218.jpg' />
         </div>
       </div>
+      <input onChange={handleChange} placeholder='add image url here.' />
+      <Button
+        style={{ zIndex: '13' }}
+        onClick={() => exportComponentAsJPEG(box)}
+        variant="contained"
+        color="primary"
+        size="large"
+        startIcon={<SaveIcon />}
+      >
+        Export as JPEG
+      </Button>
     </div>
+
   );
 }
 
--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,27 +1,44 @@
-import React, { useState, useRef } from 'react'
-import DragResizeContainer from 'react-drag-resize';
+import React, { useState } from 'react'
+import { Textfit } from 'react-textfit';
+import { Resizable, ResizableBox } from "react-resizable";
+import { Rnd } from 'react-rnd';
 
-function Title(ref) {
-      // const ref = useRef()
-      const [isOver, setOver] = useState(false)
-      const [font, setFont] = useState(40)
-      function handlChange(e) {
-            console.log(ref.current.innerText.length)
-            ref.current.innerText.length > 13 ? setFont(30) : setFont(40)
-            ref.current.innerText.length > 16 ? setFont(20) : setFont(40)
+
+function Title(ref, setOver, setLong, tooLong) {
+      const [state, setstate] = useState({})
+      const [isover, setIsOver] = useState(false)
+      function handlKeyUP(e) {
+            if (e.target.innerText.length > 69) {
+                  setLong(true)
+                  setstate({ right: '0px', width: '500px !important', background: 'linear-gradient(90deg, rgba(202,51,39,1) 0%, rgba(11,15,28,1) 100%)', borderRadius: '10px', width: ' 100 %' })
+            }
       }
-
       return (
-            <div onMouseOver={e => setOver(true)} onMouseLeave={e => setOver(false)}
+            <div
+                  style={{ ...state }}
+                  onMouseLeave={() => setOver(true)} onMouseOverCapture={() => setOver(false)}
                   className='title-containter'>
-                  <div
-                        style={{ fontSize: `${font}px` }}
-                        onKeyUp={handlChange}
-                        ref={ref}
-                        placeholder='add text here'
-                        className='title'
-                        dir='rtl'
-                        contentEditable />
+                  <Rnd
+                        onMouseOver={e => setIsOver(true)} onMouseLeave={e => setIsOver(false)}
+                        style={{ border: isover ? '2px solid #61dafb' : 'none' }}
+                        default={{
+                              x: -177,
+                              y: 155,
+                              width: 320,
+                        }}
+                  >
+                        <Textfit
+                              mode="single">
+                              <div
+
+                                    onKeyUp={handlKeyUP}
+                                    ref={ref}
+                                    placeholder='add text here'
+                                    className='title'
+                                    dir='rtl'
+                                    contentEditable />
+                        </Textfit>
+                  </Rnd>
             </div>
       )
 }
